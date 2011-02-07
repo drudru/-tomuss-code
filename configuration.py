0df58d4e75cbeb22894e2477bc7ab37fd472366c
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2011-02-07 21:08:43 +0100

    Fix huge installation bug : lost 'referents_students.py' table

--- configuration.py
+++ configuration.py
@@ -23,7 +23,7 @@ import socket
 import os
 import time
 
-version = '2.12.16'
+version = '2.12.17'
 
 # The following information can be redefined by python modules
 # loaded at the end of this file.
