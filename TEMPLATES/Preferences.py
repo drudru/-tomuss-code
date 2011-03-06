531d298bfe5eadc7b67c89eec08fb29ec876042f
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2011-03-06 21:53:42 +0100

    'Preferences' page no more displays empty lines

--- TEMPLATES/Preferences.py
+++ TEMPLATES/Preferences.py
@@ -22,29 +22,21 @@
 import data
 import utilities
 
-# DO NOT CHANGE THE ORDER IN THIS LIST TO CHANGE ORDER ON SCREEN.
-# If you want to do so, give a correct number
-# in the second column because the display is sorted with this column
-prefs = (
-('display_tips'   ,0  ,"Affiche les bulles d'aides"                    ,"OUI"),
-('display_picture',9.9  ,""                     ,""),
-('unmodifiable'   ,9.9  ,"", ""),
-('tipfixed'       ,9.9  ,""     ,""),
-('nr_favorites'   ,2.5,"Page d'accueil : nombre d'UE favorites affichées"               ,"6"  ),
-('nr_lines'       ,3  ,"Nombre de lignes affichées par défaut"         ,"0"  ),
-('nr_cols'        ,4  ,"Nombre de colonnes affichées par défaut"       ,"0"  ),
-('zebra_step'     ,4.5,"Nombres de lignes entre les traits épais"      ,"5"  ),
-('page_step'      ,4.6,"Avec 0,5 «page suivante» décale d'½ page"      ,"1"  ),
-('invert_name'    ,5  ,"Inverse les colonnes NOM et PRÉNOM"            ,"OUI"),
-('v_scrollbar'    ,6  ,"Affiche l'ascenseur vertical"                  ,"OUI"),
-('v_scrollbar_nr' ,7  ,"Nombre de caractères affichés dans l'ascenseur vertical","1"  ),
-('scrollbar_right',7.05,"Affiche l'ascenseur vertical à droite"        ,"OUI"),
-('interface'      ,7.1,"Interface utilisateur : N:normal L:lineaire (pour malvoyant)"   ,"N"),
-('favoris'        ,9.9  ,""   ,""),
-('master_of'      ,9.9  ,""   ,"[]"),
-('current_suivi'  ,10   ,"Page d'accueil : on fait le suivi du semestre courant au lieu du semestre choisi","NON"),
-('favoris_sort'  ,11   ,"Page d'accueil : trie les favoris par code","NON"),
-)
+prefs = {
+'display_tips'   :("Affiche les bulles d'aides"                       ,"OUI"),
+'nr_favorites'   :("Page d'accueil : nombre d'UE favorites affichées" ,"6"  ),
+'nr_lines'       :("Nombre de lignes affichées par défaut"            ,"0"  ),
+'nr_cols'        :("Nombre de colonnes affichées par défaut"          ,"0"  ),
+'zebra_step'     :("Nombre de lignes entre les traits épais"          ,"5"  ),
+'page_step'      :("Avec 0,5 «page suivante» décale d'½ page"         ,"1"  ),
+'invert_name'    :("Inverse les colonnes NOM et PRÉNOM"               ,"OUI"),
+'scrollbar_right':("Affiche l'ascenseur vertical à droite"            ,"OUI"),
+'favoris_sort'   :("Page d'accueil : trie les favoris par code"       ,"NON"),
+'v_scrollbar'    :("Affiche l'ascenseur vertical"                     ,"OUI"),
+'v_scrollbar_nr' :("Nombre de caractères affichés dans l'ascenseur vertical","1"  ),
+'interface'      :("Interface utilisateur : N:normal L:lineaire (pour malvoyant)"   ,"N"),
+'current_suivi'  :("Page d'accueil : on fait le suivi du semestre courant au lieu du semestre choisi","NON"),
+}
 
 def create(table):
     p = table.new_page('' ,data.ro_user, '', '')
@@ -54,11 +46,11 @@ def create(table):
     table.column_change(p,'0_2','Ordre'              ,'Text','','','F',1,2 )
     table.column_change(p,'0_3','Votre choix'        ,'Text','','','F',0,4 )
     table.table_attr(p, 'default_nr_columns', 3)
-    table.table_attr(p, 'default_sort_column', 2)
+    table.table_attr(p, 'default_sort_column', 0)
     table.new_page('' ,utilities.module_to_login(table.ue), '', '')
 
 def init(table):
-    table.default_sort_column = 2 # compatibility with old Preferences files
+    table.default_sort_column = 0 # compatibility with old Preferences files
     table.private = 1
 
 def check(table):
@@ -66,31 +58,31 @@ def check(table):
     p_rw = table.pages[1]
 
     table.lock()
-
     try:
-        for line, order, text, value in prefs:
-            if line not in table.lines:
-                if text == '':
-                    continue # Don't add obsolete preferences
-                # Do not change user entered value
-                table.cell_change(p_rw, '0_3', line, value)
-            # Create or update other values
-            table.cell_change(p_ro, '0_0', line, text)
-            table.cell_change(p_ro, '0_1', line, value)
-            table.cell_change(p_ro, '0_2', line, str(order))
-            if text == '':
-                # Remove obsolet values
-                table.cell_change(p_rw, '0_3', line, '')
+        # Remove old attribute            
+        for lin in table.lines:
+            if lin not in prefs:
+                if table.lines[lin][0].value:
+                    table.cell_change(p_ro, '0_0', lin, '')
+                    table.cell_change(p_ro, '0_1', lin, '')
+                    table.cell_change(p_ro, '0_2', lin, '')
+                    table.cell_change(p_ro, '0_3', lin, '')
+
+        # Add/update new attributes
+        for lin, value in prefs.items():
+            table.cell_change(p_ro, '0_0', lin, value[0])
+            table.cell_change(p_ro, '0_1', lin, value[1])
+            if lin not in table.lines:
+                table.cell_change(p_rw, '0_3', lin, value[1])
     finally:
         table.unlock()
 
 def preferences(table):
     p = {}
-    for line, order, text, value in prefs:
-        if line != '':
-            if line in table.lines:
-                p[line] = table.lines[line][3].value
-            else:
-                p[line] = value
+    for lin in prefs:
+        if lin in table.lines:
+            p[lin] = table.lines[lin][3].value
+        else:
+            p[lin] = prefs[lin][1]
     return p
         
