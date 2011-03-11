4c4d7f866d31389a4d7618522fdda800aed6441a
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2011-03-11 16:26:18 +0100

    Compute '' in place of 'Inconnu' in 'Surname', 'Firstname'...

--- COLUMN_TYPES/firstname.py
+++ COLUMN_TYPES/firstname.py
@@ -26,4 +26,4 @@ class Firstname(mail.Mail):
     full_title = 'Prénom(ID)'
 
     def get_one_value(self, student_id, column):
-        return inscrits.L_slow.firstname_and_surname(student_id)[0].encode('utf8')
+        return inscrits.L_slow.firstname_and_surname(student_id)[0].encode('utf8').replace('Inconnu','')
--- COLUMN_TYPES/phone.py
+++ COLUMN_TYPES/phone.py
@@ -26,4 +26,4 @@ class Phone(mail.Mail):
     full_title = 'Téléphone(ID)'
 
     def get_one_value(self, student_id, column):
-        return inscrits.L_slow.phone(student_id).encode('utf8')
+        return inscrits.L_slow.phone(student_id).encode('utf8').replace('Inconnu','')
