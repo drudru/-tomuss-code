ca9e8b639e3f9502e5ae046732e721078116cad9
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2011-10-26 11:18:03 +0200

    Firstname/Surname column types are no more in uppercase

--- COLUMN_TYPES/firstname.py
+++ COLUMN_TYPES/firstname.py
@@ -26,4 +26,4 @@ class Firstname(mail.Mail):
     full_title = 'Prénom(ID)'
 
     def get_one_value(self, student_id, column, line_id):
-        return inscrits.L_slow.firstname_and_surname(student_id)[0].encode('utf8').replace('Inconnu','')
+        return inscrits.L_slow.firstname_and_surname(student_id)[0].title().encode('utf8').replace('Inconnu','')
