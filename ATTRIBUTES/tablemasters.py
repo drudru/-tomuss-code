c48e596f6eccffaa210b6e567db7cdd1505dd062
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2011-12-19 22:25:07 +0100

    On tables without master, the GUI now stops impossible modification

--- ATTRIBUTES/tablemasters.py
+++ ATTRIBUTES/tablemasters.py
@@ -42,7 +42,7 @@ else
 if ( teachers.length )
     i_am_the_teacher = myindex(teachers, my_identity) != -1 ;
 else
-    i_am_the_teacher = true ;
+    i_am_the_teacher = false ;
 return value ;
 }'''
     tip = """Les LOGINS d'enseignants ayant tous les droits sur la table.<br>
