0b4fd64314e31948c0704938209b5fd0f412aec7
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2012-07-21 20:06:05 +0200

    'suivi_teacher' translation and virtualization

--- PLUGINS/suivi_teachers.py
+++ PLUGINS/suivi_teachers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #    TOMUSS: The Online Multi User Simple Spreadsheet)
-#    Copyright (C) 2008,2009 Thierry EXCOFFIER, Universite Claude Bernard
+#    Copyright (C) 2008-2012 Thierry EXCOFFIER, Universite Claude Bernard
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -20,19 +20,18 @@
 #    Contact: Thierry.EXCOFFIER@bat710.univ-lyon1.fr
 
 import plugin
-import configuration
 import document
 import utilities
 import referent
 import data
 from tablestat import TableStat, les_ues
+from cell import CellEmpty, CellValue, Line
+import column
 
-def teachers_statistics_table(year, semester):
-    utilities.warn('start', what='debug')
-    filename = document.table_filename(year, semester, 'teachers')
-
+def teachers_statistics(server):
+    """Create a table of statistics about all the teachers"""
     teachers = {'': TableStat('')}
-    for t in les_ues(year, semester):
+    for t in les_ues(server.year, server.semester):
         for c in t.columns:
             user_name = c.author
             if user_name not in teachers:
@@ -50,7 +49,7 @@ def teachers_statistics_table(year, semester):
             for v in line[6:]:
                 teachers[v.author].update(v, table=t)
 
-    year = utilities.university_year(year, semester)
+    year = utilities.university_year(server.year, server.semester)
     for t in referent.les_blocsnotes(year):
         for p in t.pages:
             if p.user_name not in teachers:
@@ -80,91 +79,83 @@ def teachers_statistics_table(year, semester):
         max_cels = max_cols = max_pages = max_students = max_blocnote = max_comment = 1
 
 
-        
+    columns = [
+        column.Column('c0', '', freezed='F', type='Text', width=6,
+                      title=server._('COL_TITLE_teacher'),
+                      comment=server._('COL_COMMENT_teacher'),
+                      ),
+        column.Column('cfn', '', freezed='F', type='Text', width=4,
+                      title=server._('COL_TITLE_firstname'),
+                      ),
+        column.Column('csn', '', freezed='F', type='Text', width=4,
+                      title=server._('COL_TITLE_surname'),
+                      ),        
+        column.Column('c1', '', type='Note', width=2,
+                      title=server._('COL_TITLE_nb_cells_entered'),
+                      comment=server._('COL_COMMENT_nb_cells_entered'),
+                      minmax="[0;%s]" % max_cels,
+                      ),
+        column.Column('c2', '', type='Note', width=2,
+                      title=server._('COL_TITLE_nb_columns'),
+                      comment=server._('COL_COMMENT_nb_columns'),
+                      minmax="[0;%s]" % max_cols,
+                      ),        
+        column.Column('c3', '', type='Note', width=2,
+                      title=server._('COL_TITLE_nb_pages'),
+                      comment=server._('COL_COMMENT_nb_pages'),
+                      minmax="[0;%s]" % max_pages,
+                      ),        
+        column.Column('c4', '', type='Note', width=2,
+                      title=server._('COL_TITLE_nb_comments'),
+                      comment=server._('COL_COMMENT_nb_comments'),
+                      minmax="[0;%s]" % max_comment,
+                      ),
+        column.Column('c5', '', type='Note', width=2,
+                      title=server._('COL_TITLE_nb_suivi_students'),
+                      comment=server._('COL_COMMENT_nb_suivi_students'),
+                      minmax="[0;%s]" % max_students,
+                      ),
+        column.Column('c6', '', type='Note', width=2,
+                      title=server._('COL_TITLE_nb_notepad_cells'),
+                      comment=server._('COL_COMMENT_nb_notepad_cells'),
+                      minmax="[0;%s]" % max_blocnote,
+                      ),        
+        column.Column('c7', '', type='Date', width=4,
+                      title=server._('COL_TITLE_first_change'),
+                      ),
+        column.Column('c8', '', type='Date', width=4,
+                      title=server._('COL_TITLE_last_change'),
+                      ),        
+        ]
+    table_attrs = {
+        'comment': server._('LINK_teachers'),
+        'default_nr_columns': 11
+        }
 
-    f = open(filename, "w")
-    f.write("""# -*- coding: utf8 -*-
-from data import *
-new_page('' ,'*', '', '')
-column_change (0,'0_0','Enseignant','Text','','','F',0,6)
-column_comment(0,'0_0','Identifiant (login) de l\\'enseignant')
-column_change (0,'0_1','Prénom','Text','','','F',0,4)
-column_change (0,'0_2','Nom','Text','','','F',0,4)
-column_change (0,'0_3','#cellules_saisies','Note','[0;%s]','','',0,2)
-column_comment(0,'0_3','Nombre de cellules saisies')
-column_change (0,'0_4','#colonnes_créées','Note','[0;%s]','','',0,2)
-column_comment(0,'0_4','Nombre de colonnes créées.')
-column_change (0,'0_5','#pages_vues','Note','[0;%s]','','',0,2)
-column_comment(0,'0_5','Nombre de tables affichées')
-column_change (0,'0_6','#commentaires','Note','[0;%s]','','',0,2)
-column_comment(0,'0_6','Nombre de commentaires de cellule saisis')
-column_change (0,'0_7','#Étudiants_suivis','Note','[0;%s]','','',0,2)
-column_comment(0,'0_7','Nombre d\\'étudiants suivis en tant que référent pédagogique')
-column_change (0,'0_8','#blocnote_saisie','Note','[0;%s]','','',0,2)
-column_comment(0,'0_8','Nombre de cellules saisies dans le blocnote du référent pédagogique')
-column_change (0,'0_9','Première_modification','Date','','','',0,4)
-column_change (0,'0_10','Dernière_modification','Date','','','',0,4)
-table_comment (0,'Statistiques par enseignant pour l\\'ensemble des tables')
-table_attr('default_nr_columns', 0, 11)
-""" % (max_cels, max_cols, max_pages, max_comment, max_students, max_blocnote))
+    lines = []
 
     for i, t in enumerate(teachers.values()):
         s = t.name.split('.')
         if len(s) == 1:
             s = ('', s[0])
-        f.write("""cell_change(0,'0_0','%d',%s,'')
-cell_change(0,'0_1','%d',%s,'')
-cell_change(0,'0_2','%d',%s,'')
-cell_change(0,'0_3','%d',%s,'')
-cell_change(0,'0_4','%d',%s,'')
-cell_change(0,'0_5','%d',%s,'')
-cell_change(0,'0_6','%d',%s,'')
-cell_change(0,'0_7','%d',%s,'')
-cell_change(0,'0_8','%d',%s,'')
-cell_change(0,'0_9','%d',%s,'')
-cell_change(0,'0_10','%d',%s,'')
-""" % (i, repr(t.name), i, repr(s[0]), i, repr(s[1]), i, repr(str(t.nr)),
-       i, repr(t.nr_cols), i, repr(str(t.nr_pages)),
-       i, repr(str(t.nr_comments)),
-       i, repr(t.nr_students),
-       i, repr(str(t.nr_blocnote)),
-       i, repr(t.date_min), i, repr(t.date_max)))
+        lines.append(Line((
+                    CellValue(t.name),
+                    CellValue(s[0]),
+                    CellValue(s[1]),
+                    CellValue(t.nr),
+                    CellValue(t.nr_cols),
+                    CellValue(t.nr_pages),
+                    CellValue(t.nr_comments),
+                    CellValue(t.nr_students),
+                    CellValue(t.nr_blocnote),
+                    CellValue(t.date_min),
+                    CellValue(t.date_max),
+                    )))
+    document.virtual_table(server, columns, lines, table_attrs=table_attrs)
 
-    f.close()
-
-    utilities.warn('end statistics', what='debug')
-
-
-#teachers_statistics_table = utilities.add_a_cache0(
-#    teachers_statistics_table, timeout=configuration.teacher_stat_interval)
-
-
-def teachers_stat(server):
-    """Create a table of statistics about all the teachers,
-    redirect the browser on this table."""
-    teachers_statistics_table(server.year, server.semester)
-
-
-def headers(server):    
-    return (
-        ('Location','%s/=%s/%d/%s/teachers' % (
-        configuration.server_url, server.ticket.ticket,
-        server.year, server.semester)), )
-
-plugin.Plugin('teachers', '/*',
-              function=teachers_stat,
-              teacher=True,
-              response=307,
-              headers = headers,
-              launch_thread = True,
-              link=plugin.Link(text='Statistiques enseignants',
-                               url="javascript:go_suivi('*')",
-                               where="informations",
-                                                    html_class="verysafe",
-                               help="""Pour chaque enseignant,
-                               affiche les statistiques sur
-                               les informations saisies dans TOMUSS""",
-                   ),
+plugin.Plugin('teachers', '/*', function=teachers_statistics,
+              teacher=True, launch_thread = True,
+              link=plugin.Link(url="javascript:go_suivi('*')",
+                               where="informations", html_class="verysafe",
+                               ),
               )
-
-
