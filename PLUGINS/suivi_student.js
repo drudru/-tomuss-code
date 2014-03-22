d0462658c4254f822ce7e626c9862acbb294a586
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2014-03-22 12:19:08 +0100

    Add a rank label on cellbox on 'suivi'

--- PLUGINS/suivi_student.js
+++ PLUGINS/suivi_student.js
@@ -593,9 +593,11 @@ function DisplayCellRank(node)
     
   var s = ' ' ;
   if ( DisplayGrades.cellstats.rank !== undefined)
-    s += ' ' + DisplayGrades.cellstats.rank +'/'+  DisplayGrades.cellstats.nr ;
+    s += DisplayGrades.cellstats.rank +'/'+  DisplayGrades.cellstats.nr + ' ' ;
   if ( DisplayGrades.cellstats.rank_grp !== undefined)
-    s += ' ' + DisplayGrades.cellstats.rank_grp +'/'+  DisplayGrades.cellstats.nr_in_grp ;
+    s +=DisplayGrades.cellstats.rank_grp+'/'+DisplayGrades.cellstats.nr_in_grp;
+  if ( s != ' ' )
+    s = '<span class="displaygrey">' + _("TH_rank") + '</span>' + s ;
   return s ;
 }
 DisplayCellRank.need_node = [] ;
