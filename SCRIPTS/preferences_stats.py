5ef78d21dbb8a5555a1b29afb764e997dafe77eb
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2014-04-16 11:19:59 +0200

    The preferences statistics generate a DOT file to search cluster

--- SCRIPTS/preferences_stats.py
+++ SCRIPTS/preferences_stats.py
@@ -37,6 +37,20 @@ for k in sorted(nb_on, key=lambda x: nb_on[x]):
 print 'Pairs'
 for k in sorted(pairs, key=lambda x: pairs[x]):
     print pairs[k], k
+f = '%8.8s'
+sorted_nb_on = sorted(nb_on)
+print ' '.join(f % i for i in ['',] + sorted_nb_on)
+for i in sorted_nb_on:
+    print f % i,
+    for j in sorted_nb_on:
+        if i > j:
+            print f % pairs.get((i, j), ''),
+        elif i < j:
+            print f % pairs.get((j, i), ''),
+        else:
+            print f % '',
+    print
+
 print 'Sets'
 for k in sorted(nb_set, key=lambda x: (nb_set[x], x)):
     print nb_set[k], k
@@ -46,3 +60,19 @@ for d in data:
     print ','.join(
         '1' if d.get(i,0) else '0'
         for i in nb_on)
+print 'Dot file: fdp -Tpdf xxx.dot -o xxx.pdf'
+f = open("xxx.dot", "w")
+f.write('''
+graph "fichiers" {
+node[shape=circle,style="filled",fillcolor="white",fixedsize=true];
+edge[splines="false",fontname="courier"];
+graph[charset="Latin1", orientation="P"];
+''')
+for k, v in nb_on.items():
+    f.write('%s [width="%s", label="%s"] ;\n' % (k, float(v)**0.5/3, k))
+
+for k, v in pairs.items():
+    f.write('%s -- %s [weight="%d",penwidth="%d"] ;\n' % (k[0], k[1],
+                                            v*v*v*v, v))
+f.write('}\n')
+f.close()
