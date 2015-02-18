a9c80c673894737249a5e895304b6d375dd5b096
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2015-02-18 13:36:25 +0100

    Add a column type to do simple column product

---
+++ PYTHON_JS/product.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# TOMUSS: The Online Multi User Simple Spreadsheet
+# Copyright (C) 2015 Thierry EXCOFFIER, Universite Claude Bernard
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Contact: Thierry.EXCOFFIER@univ-lyon1.fr
+
+"""
+"""
+
+def compute_product(data_col, line):
+    column = columns[data_col]
+    if len(column.average_columns) == 0:
+        return ''
+
+    nr_abj = 0
+    nr_ppn = 0
+    nr_abi = 0
+    product = 1
+    for data_column in column.average_columns:
+        value = line[data_column].value
+        origin = columns[data_column]
+        if str(origin.real_weight) != "1":
+            return _('ERROR_all_weight_equals_to_1')
+        if str(value) == '': # str is here to turn arround the JavaScript cast
+            value = origin.empty_is
+            if str(value) == '':
+                return nan # Empty cell
+        if value in (abj, abj_short):
+            nr_abj += 1
+        elif value in (ppn, ppn_short):
+            nr_ppn += 1
+        elif value in (pre, pre_short):
+            continue
+        elif value in (tnr, tnr_short, abi, abi_short):
+            nr_abi += 1
+            product = 0
+        else:
+            product *= to_float_or_nan(value)
+    if nr_abj == 0 and nr_abi != len(column.average_columns) and nr_ppn == 0:
+        return product
+    if nr_abi == len(column.average_columns):
+        return abi
+    if nr_abj == len(column.average_columns):
+        return abj
+    if nr_ppn == len(column.average_columns):
+        return ppn
+    return nan
---
+++ PYTHON_JS/product_regtest.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# TOMUSS: The Online Multi User Simple Spreadsheet
+# Copyright (C) 2015 Thierry EXCOFFIER, Universite Claude Bernard
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Contact: Thierry.EXCOFFIER@univ-lyon1.fr
+
+def product_regtest():
+    columns_set([Column(), Column(), Column(),
+                 Column({"average_columns":[0, 1, 2]}),
+               ])
+    for cells, value in [
+            [[  2,  2,  2], 8],
+            [[  2,  2,abi], 0],
+[[  2,  2,abj], nan],
+[[  2,  2,ppn], nan],
+            [[  2,  2,tnr], 0],
+            [[  2,abi,abi], 0],
+[[  2,abi,abj], nan],
+[[  2,abi,ppn], nan],
+            [[  2,abi,tnr], 0],
+[[  2,abj,abj], nan],
+[[  2,abj,ppn], nan],
+[[  2,abj,tnr], nan],
+[[  2,ppn,ppn], nan],
+[[  2,ppn,tnr], nan],
+            [[  2,tnr,tnr], 0],
+            [[abi,abi,abi], abi],
+[[abi,abi,abj], nan],
+[[abi,abi,ppn], nan],
+            [[abi,abi,tnr], abi],
+[[abi,abj,abj], nan],
+[[abi,abj,ppn], nan],
+[[abi,abj,tnr], nan],
+[[abi,ppn,ppn], nan],
+[[abi,ppn,tnr], nan],
+            [[abi,tnr,tnr], abi],
+            [[abj,abj,abj], abj],
+[[abj,abj,ppn], nan],
+[[abj,abj,tnr], nan],
+[[abj,ppn,ppn], nan],
+[[abj,ppn,tnr], nan],
+[[abj,tnr,tnr], nan],
+            [[ppn,ppn,ppn], ppn],
+[[ppn,ppn,tnr], nan],
+[[ppn,tnr,tnr], nan],
+            [[tnr,tnr,tnr], abi],
+    ]:
+        line = [Cell(cells[0]), Cell(cells[1]), Cell(cells[2]), CE(value)]
+        check_result(line, 3, compute_product)
+        
+    print('Product regtest are fine')
+
+product_regtest()
