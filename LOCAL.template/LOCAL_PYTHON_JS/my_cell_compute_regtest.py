d83240cbd7691f8e3ab70a350af0d9183d78e39d
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2016-01-22 17:15:33 +0100

    Fix the example of regression test in LOCAL.template.my_cell_compute

--- LOCAL.template/LOCAL_PYTHON_JS/my_cell_compute_regtest.py
+++ LOCAL.template/LOCAL_PYTHON_JS/my_cell_compute_regtest.py
@@ -4,7 +4,7 @@
 
 def my_cell_compute_regtest():
     columns_set([Column(), Column(), Column({"average_columns": [0, 1]})])
-    line = [Cell('x', author="z"), Cell('y', comment="C"), CE("xy")]
+    line = [Cell('x', "z"), Cell('y', "", "", "C"), CE("xy")]
     check_result(line, 2, compute_my_cell_compute)
         
     print('my_cell_compute regtest are fine')
