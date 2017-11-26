c5b54ea4d4741563187709713af59ae613f85679
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2017-11-26 11:47:21 +0100

    The suivi was broken by the last commit

--- PLUGINS/suivi_student.py
+++ PLUGINS/suivi_student.py
@@ -535,6 +535,7 @@ def suivi_headers(server, is_student=True):
         + "var year     = %s;\n" % utilities.js(server.year             )
         + "var is_a_teacher = %s;\n" % int(not is_student)
         + 'var max_visibility_date = %d;\n' % configuration.max_visibility_date
+        + 'var rounding_default = %s;\n' % configuration.rounding_default
         + 'var upload_max = %s;\n' % utilities.js(configuration.upload_max)
         + 'column_get_option_running = true ;\n'
     )
